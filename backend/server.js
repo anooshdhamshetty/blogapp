29cc0bfb60ebe7eddc0a30d9a8c9e2e340703c9a
Anoosh Dhamshetty <anooshdhamshetty@gmail.com>
2026-03-08 14:14:15 +0530

    frontend url changed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   "http://localhost:5173", 
   "http://localhost:5174", 
   "http://localhost:3000",
-  "https://blogapp-sigma-indol.vercel.app/" // Add your Vercel URL here
+  "https://blogapp-sigma-indol.vercel.app" // Add your Vercel URL here
 ].filter(Boolean); // Remove undefined values
 
 app.use(cors({
